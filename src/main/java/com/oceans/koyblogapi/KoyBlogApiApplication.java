6a33ca1d9da545548849e0f3a54363886f587602
koy <luke@koyui-MacBookPro.local>
2022-09-19 21:40:48 +0900

    init

---
+++ src/main/java/com/oceans/koyblogapi/KoyBlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.oceans.koyblogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoyBlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoyBlogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oceans/koyblogapi/KoyBlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oceans.koyblogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoyBlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
